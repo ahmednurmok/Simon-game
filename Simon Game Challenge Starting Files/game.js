298c26a0383651068d9aba5057c0ca17abd818b5
ahmednurmok <ahmednurmok@gmail.com>
2025-12-01 10:51:43 +0000

    Add files via upload

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -26,6 +26,7 @@ $(document).keypress(function() {
 
 function nextSequence() { 
     level ++;
+    userClickedPattern = []; // resets the userClickedPattern to an empty array ready for the next level.
     $("#level-title").text("Level " + level);
     return Math.floor(Math.random() * 4);
     
@@ -53,9 +54,13 @@ $("button").on("click", function() {
  //'this' references the event listener (button clicked and its attribute, e.g. green)
  userClickedPattern.push(userChosenColour);
  playSound(userChosenColour);
+
+ checkAnswer(userClickedPattern.length-1);
+
     });
 
 // this is known as a handler function. userclickedpattern notes all of the clicks
+//check answer ensures the answer matches the previous answer (e.g. if 1.red and 2. is red, yellow - checks thats 1st is red)
 
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
@@ -71,5 +76,45 @@ function animatePress(currentColour) {
 };
 
 
+function checkAnswer(currentLevel){
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+        console.log("success");
+  
+      
+        if (userClickedPattern.length === gamePattern.length){
+  
+          
+          setTimeout(function () {
+            nextSequence();
+          }, 1000);
+  
+        }
+   // If the user got the most recent answer right in step 3, then check that they have finished their sequence, with another if statement to ensure the correct number of sequence is clicked (e.g. 2 buttons for yellow,green), if someone clicks 3 they fail (else)
+   // Call nextSequence() - NEXTLEVEL after a 1000 millisecond delay.
+
+      } else {
+  
+        console.log("wrong");
+  
+      }
+    
+      playSound('wrong');  //sound
+
+      $("body").addClass("game-over");
+    setTimeout(function() {
+        $("body").removeClass("game-over");
+    }, 200); //200ms
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");  //restart
+    startOver(); //starts game again
 
 
+};
+
+startOver(){
+    level = 0;
+  gamePattern = [];
+  started = false;   //starts game from scratch
+}
